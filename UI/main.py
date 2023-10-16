810dc9c183f5e3a4a6a0bec829e4399070f36ecf
Urjit Sharma <104162053+UrjitS@users.noreply.github.com>
2023-10-16 16:20:49 -0700

    Reformatted files

--- UI/main.py
+++ UI/main.py
@@ -10,9 +10,8 @@ import tkinter.messagebox
 import customtkinter
 
 import options
-
-# from UI.receiver import create_receiver_connection
-# from UI.sender import create_sender_connection
+from UI.receiver import create_receiver_connection
+from UI.sender import create_sender_connection
 
 customtkinter.set_appearance_mode(
     "System"
