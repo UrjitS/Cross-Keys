897a7d7b97fa877f835c2c9ff05e46a847944df3
Urjit Sharma <104162053+UrjitS@users.noreply.github.com>
2023-12-06 17:25:01 -0800

    Add screen capture functionality with cursor overlay

--- UI/screen_capture.py
+++ UI/screen_capture.py
@@ -1,24 +1,29 @@
-"""
-This module is used to capture the screen of the user's computer.
-"""
+import pyautogui
+from PIL import Image
 from mss import mss
 
+# Load cursor image and convert to RGBA
+cursor_image = Image.open('cursor.png').convert('RGBA')
+
+# Scale down cursor image
+cursor_image = cursor_image.resize((100, 100))
+
+# Take screenshot
 with mss() as sct:
-    screenshot = sct.shot()
+    screenshot = sct.grab(sct.monitors[0])
 
-# def capture_screen():
-#     """
-#     This function is used to capture the screen of the user's computer.
-#     """
+# Convert screenshot to PIL Image object
+screenshot = Image.frombytes("RGB", screenshot.size, screenshot.bgra, "raw", "BGRX").convert('RGBA')
 
-#     screenshot = pyautogui.screenshot()
-#     screenshot.show()
+# Get mouse cursor position
+cursor_x, cursor_y = pyautogui.position()
 
-# def main():
-#     """
-#     This is the main function of the module.
-#     """
-#     capture_screen()
+# Create a new image with the same size as the screenshot and paste the cursor onto it
+cursor_layer = Image.new('RGBA', screenshot.size)
+cursor_layer.paste(cursor_image, (cursor_x, cursor_y))
 
-# if __name__ == "__main__":
-#     main()
+# Composite the screenshot and cursor layer
+final_image = Image.alpha_composite(screenshot, cursor_layer)
+
+# Save the screenshot
+final_image.save('screenshot_with_cursor.png')
